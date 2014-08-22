c9e662ff508e9224c4d3175d371c54a6c716b64c
201112026 <i201112026@cc.niit.ac.jp>
2014-08-22 15:34:36 +0900

    0822

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806090548) do
+ActiveRecord::Schema.define(version: 20140822050510) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140806090548) do
     t.string   "password_digest"
     t.string   "shubetu"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
